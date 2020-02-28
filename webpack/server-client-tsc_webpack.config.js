4de879fbefc65c98c9709254a84c528761028b35
haryoiro <55312590+haryoiro@users.noreply.github.com>
2020-02-28 20:39:45 +0900

    Update server-client-tsc_webpack.config.js

--- webpack/server-client-tsc_webpack.config.js
+++ webpack/server-client-tsc_webpack.config.js
@@ -1,27 +1,44 @@
 const nodeExternals = require('webpack-node-externals')
 const NodemonPlugin = require('nodemon-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
 
 const clientConfig = {
   mode: "development",
+  // mode: "production",
+  devtool: "none",
+  entry: "./src/public/index.ts",
   output: {
-    filename: "[name].js",
-    path: `${__dirname}/dist/public`
+    path: path.resolve(__dirname, 'dist/public'), // プラグインで出力される場所も設定するので、クライアント側のファイルを全て格納するパスを設定
+    filename: 'index[hash].js' // ハッシュをつけることでキャッシュによる動作不良を解消する
   },
   module: {
     rules: [{
-      text: /\.tsx?$/,
+      test: /\.tsx?$/,
       use: "ts-loader",
       exclude: /node_modules/,
     }]
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/public/index.html"　// トランスパイル元となるファイルを設定
+    })
+  ]
 }
 
 const serverConfig = {
-  mode: "development",
+  mode: "development", // <- 圧縮無効 早い
+  // mode: "production", // <- 圧縮有効 遅い
+  devtool: "none",
   entry: "./src/index.ts",
+  output: {
+    filename: `server.js`,
+    path: `${__dirname}/dist`
+  },
   target: "node",
   /* outDirはtsconfig.jsonのものが有効になる */
   node: {
