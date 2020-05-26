33a68580f2ad4ffce56f8c63963f24433826b929
haryoiro <55312590+haryoiro@users.noreply.github.com>
2020-05-26 19:03:10 +0900

    Update fish_greeting.fish

--- fish_mac/functions/fish_greeting.fish
+++ fish_mac/functions/fish_greeting.fish
@@ -1,15 +1,24 @@
+
 # -----------------------------
 # Cutie Cat greeting
 # -----------------------------
 function fish_greeting
     set_color -o --bold white
-    echo "================================================="
-    echo "                                                 "
-    echo "           =^._.^=  Welcome to fish              "
-    echo "                                                 "
+    echo -ne "\e[1m  ================================================= \n"
+    echo -ne "\e[1m |                                 "
+    date "+DATE: %D    |"
+    echo -ne "\e[1m |   =^._.^=  Welcome to fish                        |\n"
+    echo -ne "\e[1m |                                 "
+    date "+TIME: %T    |"
+    echo -ne "\e[1m  ================================================= \r"
+    echo -ne "\e[1m"
+    echo -ne "\r\e[1m  =====\e[34m\e[0m"
+    echo -ne "\n\r └─> \e[1m"
     pwd
-    exa
-    echo "                                                 "
-    echo "================================================="
-    set_color normal
+    echo -ne "\r\n "
+    exa -s type --git-ignore --blocks --icons -a -G
+    echo -ne "                                                 \n"
+    echo -ne "  ================================================= \n\n"
+    echo -ne "\e[1m"
+end
 end
