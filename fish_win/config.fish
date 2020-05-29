8c993f809babc434d6a4f2c241398d94445decb1
haryoiro <55312590+haryoiro@users.noreply.github.com>
2020-05-29 14:08:51 +0900

    Update config.fish

--- fish_win/config.fish
+++ fish_win/config.fish
@@ -1,1 +1,8 @@
+set -g theme_newline_cursor yes
+set -g theme_display_git_master_branch yes
+set -g theme_color_scheme gruvbox
+set -g theme_powerline_fonts yes
+set -g theme_display_date no
+set -g theme_display_cmd_duration no
+
 builtin cd ~/../../mnt/f/Dev
