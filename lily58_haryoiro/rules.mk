d35869bb3ff22478aa8c21036143b983e5f567cc
haryoiro <55312590+haryoiro@users.noreply.github.com>
2021-12-22 14:00:50 +0900

    Update RGBLED Rules

--- lily58_haryoiro/rules.mk
+++ lily58_haryoiro/rules.mk
@@ -12,12 +12,16 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output
 UNICODE_ENABLE = no         # Unicode
-RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE= no     # OLED display
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
+
+# RGBLED SETTINGS
+RGBLIGHT_ENABLE = yes
+RGBLIGHT_DRIVER = WS2812
+WS2812_DRIVER = bitbang
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
